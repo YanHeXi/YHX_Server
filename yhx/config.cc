75475ed802154604057e9f768de7ba32ac0f5822
YanHeXi <1165333873@qq.com>
2023-12-13 10:16:53 +0800

    update

--- yhx/config.cc
+++ yhx/config.cc
@@ -22,18 +22,15 @@ namespace yhx
     //  B: 10
     //  C: str
 
-    static void ListAllMember(const std::string &prefix
-                              //   const YAML::Node &node,
-                              //   std::list < std::pair < std::string,
-                              //   const YAML::Node>> &output
-    )
+    static void ListAllMember(const std::string &prefix,
+                              const YAML::Node &node,
+                              std::list<std::pair<std::string, const YAML::Node>> &output)
     {
-        if (prefix.find_first_not_of("abcdefghikjlmnopqrstuvwxyz._012345678") != std::string::npos)
-        {
-            YHX_LOG_ERROR(g_logger) << "Config invalid name: " << prefix << " : ";
-            //  << node;
-            return;
-        }
+        // if (prefix.find_first_not_of("abcdefghikjlmnopqrstuvwxyz._012345678") != std::string::npos)
+        // {
+        //     YHX_LOG_ERROR(g_logger) << "Config invalid name: " << prefix << " : " << node;
+        //     return;
+        // }
         // output.push_back(std::make_pair(prefix, node));
         // if (node.IsMap())
         // {
@@ -47,37 +44,37 @@ namespace yhx
         // }
     }
 
-    // void Config::LoadFromYaml(const YAML::Node &root)
-    // {
-    //     std::list<std::pair<std::string, const YAML::Node>> all_nodes;
-    //     ListAllMember("", root, all_nodes);
+    void Config::LoadFromYaml(const YAML::Node &root)
+    {
+        // std::list<std::pair<std::string, const YAML::Node>> all_nodes;
+        // ListAllMember("", root, all_nodes);
 
-    //     for (auto &i : all_nodes)
-    //     {
-    //         std::string key = i.first;
-    //         if (key.empty())
-    //         {
-    //             continue;
-    //         }
+        // for (auto &i : all_nodes)
+        // {
+        //     std::string key = i.first;
+        //     if (key.empty())
+        //     {
+        //         continue;
+        //     }
 
-    //         std::transform(key.begin(), key.end(), key.begin(), ::tolower);
-    //         ConfigVarBase::ptr var = LookupBase(key);
+        //     std::transform(key.begin(), key.end(), key.begin(), ::tolower);
+        //     ConfigVarBase::ptr var = LookupBase(key);
 
-    //         if (var)
-    //         {
-    //             if (i.second.IsScalar())
-    //             {
-    //                 var->fromString(i.second.Scalar());
-    //             }
-    //             else
-    //             {
-    //                 std::stringstream ss;
-    //                 ss << i.second;
-    //                 var->fromString(ss.str());
-    //             }
-    //         }
-    //     }
-    // }
+        //     if (var)
+        //     {
+        //         if (i.second.IsScalar())
+        //         {
+        //             var->fromString(i.second.Scalar());
+        //         }
+        //         else
+        //         {
+        //             std::stringstream ss;
+        //             ss << i.second;
+        //             var->fromString(ss.str());
+        //         }
+        //     }
+        // }
+    }
 
     static std::map<std::string, uint64_t> s_file2modifytime;
     // static yhx::Mutex s_mutex;
