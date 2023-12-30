0dcbe168ebc19c9ce046cada611226728d7906b9
YanHeXi <1165333873@qq.com>
2023-12-30 12:55:28 +0800

    update

---
+++ build/CMakeFiles/test_iomanager.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "/home/yanhexi/vscode_project/YHX_Server/bin/test_iomanager"
+  "/home/yanhexi/vscode_project/YHX_Server/bin/test_iomanager.pdb"
+  "CMakeFiles/test_iomanager.dir/tests/test_iomanager.cc.o"
+  "CMakeFiles/test_iomanager.dir/tests/test_iomanager.cc.o.d"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/test_iomanager.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
